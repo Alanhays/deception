16ee2fcda713ee1b7428d7083b8e8c5aa9bc6ca9
alanhays <suran0617@163.com>
2023-02-13 14:51:18 +0800

    init

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 13/2/2023 下午2:39
+@Author  : AlanHays
+"""
+
+# from setuptools import setup, find_packages
+#
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+#
+# setup(
+#     name="deception",
+#     version="1.0.0",
+#     author="AlanHays",
+#     description="爬虫工具",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/Alanhays/deception",
+#     packages=find_packages(where='.', exclude=(), include=('*',)),
+#     classifiers=[],
+#     install_requires=[],
+#     python_requires='<3.11',
+#     include_package_data=True,
+#     install_package_data=True,
+# )
